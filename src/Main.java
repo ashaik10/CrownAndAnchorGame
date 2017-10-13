cb11b5ed450625d3d21faaa73454c2737a00e697
mjaved06 <mjaved.jazz@gmail.com>
2017-10-13 18:54:05 +1100

    Bug 01 Fixed
    Roll Over With no spin

--- src/Main.java
+++ src/Main.java
@@ -79,5 +79,25 @@ public class Main {
         
         System.out.println(String.format("Overall win rate = %.1f%%", (float)(totalWins * 100) / (totalWins + totalLosses)));
 	}
+// Bug 01 fixation roll over with no spin 
+    private static boolean checkNoSpinThrow(List<DiceValue> cdv) {
+	    boolean success = false;
+	    for (int i = 0; i <cdv.size(); i++){
+	        if(cdv.get(i).toString().equals("Anchor") ||
+                    cdv.get(i).toString().equals("Crown") ||
+                    cdv.get(i).toString().equals("Heart") ||
+                    cdv.get(i).toString().equals("Diamond") ||
+                    cdv.get(i).toString().equals("Club") ||
+                    cdv.get(i).toString().equals("Spade")){
+	            success = true;
+            }
+            else success = true;
+            if(!success) {
+                return false;
+            }
+        }
+	    return true;
+    }
+
 
 }
