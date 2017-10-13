3e101567c6ccbfda365ceb94ce834e9aef1f4ca8
mjaved06 <mjaved.jazz@gmail.com>
2017-10-13 23:02:29 +1100

    bug 06
    issue fixation

--- src/Main.java
+++ src/Main.java
@@ -103,7 +103,11 @@ public class Main {
        
         } //while true
         
+        	// Issue 06 Fixation 
+		
+        
         System.out.println(String.format("Overall win rate = %.1f%%", (float)(totalWins * 100) / (totalWins + totalLosses)));
+	}
  //Bug 01 fixation roll over with no spin 
     private static boolean checkNoSpinThrow(List<DiceValue> cdv) {
 	    boolean success = false;
